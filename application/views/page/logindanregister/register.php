9f1e2bc81205dd3dfe2afdb1ddf264590497b086
Fachreza Maulana <zzafachreza@gmail.com>
2024-10-18 13:53:39 +0700

    perbaikan stashed

--- application/views/page/logindanregister/register.php
+++ application/views/page/logindanregister/register.php
@@ -207,7 +207,7 @@
     </style>
 </head>
 <?php
-
+// unutk mobile
 function isMobile() {
     return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 }
